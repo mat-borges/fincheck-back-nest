66d04ea68f190ab5e782ce0e0edd8402b4647e6f
Mateus Borges <borgesm.work@gmail.com>
2025-07-13 20:55:07 -0300

    feat: add seeding functionality for categories in the database

---
+++ src/seeds/category.seed.ts
@@ -0,0 +1,30 @@
+import { Category } from 'src/modules/category/category.entity';
+import { DataSource } from 'typeorm';
+import { Logger } from '@nestjs/common';
+
+export const seedCategories = async (dataSource: DataSource) => {
+  const categoryRepo = dataSource.getRepository(Category);
+  const logger = new Logger('CategorySeeder');
+
+  const defaultCategories = [
+    'Alimentação',
+    'Mercado',
+    'Saúde',
+    'Lazer',
+    'Educação',
+    'Transporte',
+    'Poupança',
+    'Salário',
+    'Assinaturas',
+    'Outros',
+  ];
+
+  for (const name of defaultCategories) {
+    const exists = await categoryRepo.findOne({ where: { name } });
+    if (!exists) {
+      await categoryRepo.save({ name });
+    }
+  }
+
+  logger.log('✅ Categories seeded');
+};
---
+++ src/seeds/main.ts
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+
+import { Category } from 'src/modules/category/category.entity';
+import { DataSource } from 'typeorm';
+import { Logger } from '@nestjs/common';
+import { seedCategories } from './category.seed';
+
+dotenv.config({
+  path: `.env.stage.${process.env.STAGE || 'dev'}`,
+});
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [Category],
+  synchronize: true,
+});
+
+async function bootstrap() {
+  const logger = new Logger('Seeding');
+
+  try {
+    await AppDataSource.initialize();
+    await seedCategories(AppDataSource);
+    await AppDataSource.destroy();
+    logger.log('🌱 Seeding finished');
+  } catch (err) {
+    logger.error('❌ Seeding failed', err);
+    process.exit(1);
+  }
+}
+
+void bootstrap();
