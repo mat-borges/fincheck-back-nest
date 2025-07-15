87fa2edb874249c9d57e798b3947b6a73eb5de3e
Mateus Borges <borgesm.work@gmail.com>
2025-07-14 22:20:34 -0300

    feat: enable CORS for the application to allow requests from the specified origin

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
+    app.enableCors({ origin: 'http://localhost:5173', credentials: true });
     await app.listen(process.env.PORT ?? 3000);
 
     logger.log(`✅ Application running on Port: ${process.env.PORT ?? 3000}`);
