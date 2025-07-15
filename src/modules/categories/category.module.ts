185452e8e383003902a244f30e0b02e625b5c4ad
Mateus Borges <borgesm.work@gmail.com>
2025-07-14 22:21:10 -0300

    feat: add CategoryRepository to CategoryModule providers for enhanced data management

--- src/modules/categories/category.module.ts
+++ src/modules/categories/category.module.ts
@@ -1,12 +1,13 @@
 import { Category } from './category.entity';
 import { CategoryController } from './category.controller';
+import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
-  providers: [CategoryService],
+  providers: [CategoryService, CategoryRepository],
   controllers: [CategoryController],
 })
 export class CategoryModule {}
