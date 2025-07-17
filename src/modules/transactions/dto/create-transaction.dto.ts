6ea52aeba2bc04482b8354a7257b1e816f6ebdca
Mateus Borges <borgesm.work@gmail.com>
2025-07-17 19:19:09 -0300

    fix: create transaction DTO to match the update transaction DTO

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,18 +1,24 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsUUID } from 'class-validator';
+
+import { Transform } from 'class-transformer';
 
 export class CreateTransactionDto {
   @IsNotEmpty()
   title!: string;
 
+  @IsOptional()
   description?: string;
 
   @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => Number(value))
+  @IsNumber()
   amount!: number;
 
   @IsNotEmpty()
   date!: string;
 
   @IsNotEmpty()
+  @IsEnum(['income', 'expense'])
   type!: 'income' | 'expense';
 
   @IsUUID()
